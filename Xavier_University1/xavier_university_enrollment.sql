ffe9471bb4f8be67cd5f903a508886438c8b3f77
Austin Moore <austin19moore@gmail.com>
2023-04-24 23:36:24 +0000

    Update xavier_university_enrollment.sql

--- Xavier_University1/xavier_university_enrollment.sql
+++ Xavier_University1/xavier_university_enrollment.sql
@@ -7,5 +7,5 @@ CREATE TABLE `ENROLLMENT` (
     `ENROLL_GRADE` decimal(4,2) DEFAULT NULL, -- Grade received in class, only if the semester is over. Otherwise it's the grade the professor has given the student so far.
     PRIMARY KEY (`ENROLL_ID`),
     FOREIGN KEY (`STU_ID`) REFERENCES STUDENT(`STU_ID`),
-    FOREIGN KEY (`CLASS_ID`) REFERENCES CLASS(`CLASS_ID`),
+    FOREIGN KEY (`CLASS_ID`) REFERENCES CLASS(`CLASS_ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
